abb2db878e507d3e32b8689ae6a07a6b9b052f94
deeproker <94794466+deeproker@users.noreply.github.com>
2024-12-23 16:01:21 +0100

    Add files via upload

--- build/lib/src/spark_session_manager.py
+++ build/lib/src/spark_session_manager.py
@@ -1,4 +1,9 @@
+import logging
 from pyspark.sql import SparkSession
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 class SparkSessionManager:
     _instance = None
@@ -7,7 +12,16 @@ class SparkSessionManager:
     def get_spark_session(cls):
         """Get a singleton Spark session."""
         if cls._instance is None:
-            cls._instance = SparkSession.builder \
-                .appName("KLM Data Analysis") \
-                .getOrCreate()
+            try:
+                logger.info("Creating a new Spark session.")
+                cls._instance = SparkSession.builder \
+                    .appName("KLM Data Analysis") \
+                    .getOrCreate()
+                logger.info("Spark session created successfully.")
+            except Exception as e:
+                logger.error("Error while creating Spark session: %s", e, exc_info=True)
+                raise
+        else:
+            logger.info("Returning existing Spark session.")
+        
         return cls._instance
